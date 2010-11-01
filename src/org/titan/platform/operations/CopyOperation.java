a78c5bdd1daf1e10788db9575197a0a900fb13d8
rafaelferencz <rafaelferencz@gmail.com>
2010-11-01 17:54:05 -0300

    correcao de conflito.

--- src/org/titan/platform/operations/CopyOperation.java
+++ src/org/titan/platform/operations/CopyOperation.java
@@ -7,40 +7,35 @@ package org.titan.platform.operations;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
+import java.util.Collections;
 import java.util.List;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.CopyOperationImplementation;
 import org.openide.filesystems.FileObject;
 
+import org.titan.platform.TitanPlatformProject;
 
-/**
- *
- * @author rafael
- */
-public class CopyOperation implements CopyOperationImplementation {
+public final class CopyOperation implements CopyOperationImplementation {
 
-    @Override
-    public void notifyCopying() throws IOException {
-    }
+        private final TitanPlatformProject project;
+        private final FileObject projectDir;
 
-    @Override
-    public void notifyCopied(Project prjct, File file, String string) throws IOException {
-    }
+        public CopyOperation(TitanPlatformProject project) {
+            this.project = project;
+            this.projectDir = project.getProjectDirectory();
+        }
 
-    @Override
-    public List<FileObject> getMetadataFiles() {
-           List<FileObject> dataFiles = new ArrayList<FileObject>();
-        return dataFiles;
-    }
+        public List<FileObject> getMetadataFiles() {
+            return Collections.EMPTY_LIST;
+        }
 
-    @Override
-    public List<FileObject> getDataFiles() {
-         List<FileObject> dataFiles = new ArrayList<FileObject>();
-        return dataFiles;
-    }
+        public List<FileObject> getDataFiles() {
+            return Collections.EMPTY_LIST;
+        }
 
+        public void notifyCopying() throws IOException {
+        }
 
-
+        public void notifyCopied(Project arg0, File arg1, String arg2) throws IOException {
+        }
 }
